bda2bd1af22ce727d7536423f5f8fcfb9c96a075
Jeff Ripke <jripke74@gmail.com>
2017-03-14 14:59:56 -0700

    Algorithms Plus Minu initial

---
+++ swift/algorithmsPlusMinus.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+// number of elements
+let n = Int(readLine()!)!
+
+// read array and map the elements to integer
+let arr = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+
+
+var positiveNumberCount = 0
+var negativeNumberCount = 0
+var zeroCount = 0
+
+var percentOfPositiveNumbers = 0.0
+var percentOfNegativeNumbers = 0.0
+var percentOfZeros = 0.0
+
+for number in arr {
+  if number < 0 {
+    negativeNumberCount += 1
+  } else if number == 0 {
+    zeroCount += 1
+  } else {
+    positiveNumberCount += 1
+  }
+}
+
+percentOfPositiveNumbers = Double(positiveNumberCount) / Double(n)
+percentOfNegativeNumbers = Double(negativeNumberCount) / Double(n)
+percentOfZeros = Double(zeroCount) / Double(n)
+
+print(String(format: "%5.6f", percentOfPositiveNumbers))
+print(String(format: "%5.6f", percentOfNegativeNumbers))
+print(String(format: "%5.6f", percentOfZeros))
